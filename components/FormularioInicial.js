a2c3fb9aa7906e97babec81523c41ad5549e7e0c
Julio Cartier <c1327805@interno.bb.com.br>
2023-10-02 16:56:07 -0300

    Fazendo o primeiro commit

---
+++ components/FormularioInicial.js
@@ -0,0 +1,10 @@
+import React, {Component} from "react";
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function FormularioInicial() {
+  return (
+    <View>
+      <Text>Texto Teste</Text>
+    </View>
+  );
+}
