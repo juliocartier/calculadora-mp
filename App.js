0017c691f5c811a717a4489116dc39b0db5e6297
Elizeu Sandro <elizeusandro3@gmail.com>
2024-04-17 12:33:39 -0300

    Inserção do StatusBar

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Slider from './components/Slider';
 import FormularioInicial from './components/FormularioInicial';
+import { StatusBar } from 'expo-status-bar';
 
 const Stack = createStackNavigator();
 
@@ -10,9 +11,22 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Slider">
-        <Stack.Screen name="Slider" component={Slider} />
-        <Stack.Screen name="FormularioInicial" component={FormularioInicial} />
+        <Stack.Screen 
+          name="Slider" 
+          component={Slider} 
+          options={{
+            headerShown: false, // Para ocultar o título "Slider" no header
+          }}
+        />
+        <Stack.Screen 
+          name="FormularioInicial" 
+          component={FormularioInicial} 
+          options={{
+            title: 'Formulário Inicial', // Título para a tela de FormularioInicial no header
+          }}
+        />
       </Stack.Navigator>
+      <StatusBar style="auto" />
     </NavigationContainer>
   );
 }
